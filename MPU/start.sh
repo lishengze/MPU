ef3b10cb63beb77d2b9c1085e91699758b35df66
lishengze <lishengz.cool@hotmail.com>
2021-08-26 20:52:30 +0800

    delele files

--- MPU/start.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-nohup python3 Entry_MarketV2.py PRODUCTION &
-
-sleep 1s
-
-ps -aux|grep python3
--- MPU/stop.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-
-PID=`ps aux|grep Entry_MarketV2.py |grep -v grep | awk '{print $2}'` 
-kill -9 $PID
-
-sleep 1s
-
-ps aux|grep python3
