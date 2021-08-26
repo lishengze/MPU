50302453b3668af3228cc68d390d4943a9bf7d7e
lishengze <lishengz.cool@hotmail.com>
2021-08-26 21:22:07 +0800

    try bugs

--- MPU/MarketV2_FTX.py
+++ MPU/MarketV2_FTX.py
@@ -103,7 +103,7 @@ class FTX(object):
         if redis_config is None:
             self._redis_config = get_redis_config()
 
-        self.__publisher = Publisher(exchange=self.__exchange_name, redis_config=self._redis_config, debug_mode=debug_mode)
+        self.__publisher = Publisher(exchange=self.__exchange_name, redis_config=self._redis_config, debug_mode=debug_mode, logger=self._logger)
         self._publish_count_dict = {
             "depth":{},
             "trade":{},
