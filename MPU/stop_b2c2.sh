53cf0af4a07245979197100408ed1e98abb7d128
lishengze <lishengz.cool@hotmail.com>
2021-10-29 10:56:45 +0800

    update stop sh

--- MPU/stop_b2c2.sh
+++ MPU/stop_b2c2.sh
@@ -1,11 +1,13 @@
 # !/bin/bash
 
+rm -fr log/B2C2/*.log*
+
 PID=`ps aux|grep B2C2 |grep -v grep | awk '{print $2}'`
 kill -9 $PID
 
 sleep 2s
 
 
-rm -fr log/B2C2/*.log
+
 
 ps -aux|grep python3
--- XService/stop.sh
+++ XService/stop.sh
@@ -1,5 +1,7 @@
 # !/bin/bash
 
+rm -fr log/*.log*
+
 PID=`ps aux|grep XKLine.py  |grep -v grep | awk '{print $2}'` 
 kill -9 $PID
 
