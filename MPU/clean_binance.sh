3002231fa1931ce8e9278142a8f01a6500048aa1
lishengze <lishengz.cool@hotmail.com>
2022-01-13 11:16:38 +0800

    try

--- MPU/clean_binance.sh
+++ MPU/clean_binance.sh
@@ -1,10 +1,12 @@
 # !/bin/bash
 
+rm -fr log/BINANCE/*.log.*
+
 PID=`ps aux|grep binance |grep -v grep | awk '{print $2}'`
 kill -9 $PID
 
 sleep 2s
 
-rm -fr log/BINANCE/*.log.*
+
 
 ps -aux|grep python3
--- MPU/clean_huobi.sh
+++ MPU/clean_huobi.sh
@@ -1,10 +1,12 @@
 # !/bin/bash
 
+rm -fr log/HUOBI/*.log.*
+
+
 PID=`ps aux|grep huobi |grep -v grep | awk '{print $2}'`
 kill -9 $PID
 
 sleep 2s
 
-rm -fr log/HUOBI/*.log.*
 
 ps -aux|grep python3
