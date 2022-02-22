dd50977c5745faa4e5f921d43e1bbd2a49d7ed4d
lishengze <lishengz.cool@hotmail.com>
2022-02-22 12:50:27 +0800

    set kline redis

--- XService/start.sh
+++ XService/start.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-nohup  python3 /mnt/market/XService/XKLine.py 60 PRODUCTION > /mnt/market/XService/log/xkline.log  &
+nohup  python3 /mnt/market/XService/XKLine_redis.py 60 PRODUCTION > /mnt/market/XService/log/xkline.log  &
 
 sleep 1s
 
