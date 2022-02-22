c743e59055f5e0368e623062a44158758b47ab26
lishengze <lishengz.cool@hotmail.com>
2022-02-22 12:48:26 +0800

    set redis

--- MPU/MarketV2_FTX.py
+++ MPU/MarketV2_FTX.py
@@ -339,7 +339,7 @@ class FTX(object):
             self._logger._logger.warning(str(e))
 
 def test_ftx():
-    ftx_obj = FTX(debug_mode=False, is_redis=False)
+    ftx_obj = FTX(debug_mode=False, is_redis=True)
     ftx_obj.start()
 
 if __name__ == "__main__":
